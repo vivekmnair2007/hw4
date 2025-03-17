4510cdb13bd738caf8da5bd4dd9d55c602ccaed0
Vivek Nair <vivek.nair@kellogg.northwestern.edu>
2025-03-17 22:56:41 +0000

    Assignment 4 initial changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,12 +10,18 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+  # Login/logout
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
 end
